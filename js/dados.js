721021b812efcd34200614fa5cdb2337c4caf909
Raul <106830985+crewskt@users.noreply.github.com>
2023-12-15 10:25:57 -0300

    1

---
+++ js/dados.js
@@ -0,0 +1,114 @@
+var MENU = {
+    
+    "burgers": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/sobremesas/brigadeiro.png",
+            "name": "Brigadeiros",
+            "dsc": " Guirlandinha de Brigadeiros presenteável composta de 6 brigadeiros gourmet.",
+            "price": 22
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/sobremesas/cxdebrigadeiro.jpg",
+            "name": "Cx. de Brigadeiros",
+            "dsc": "Caixa presenteável com 12 brigadeiros gourmet (consulte sabores).",
+            "price": 40
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "./img/cardapio/sobremesas/barranatalina.jpg",
+            "name": "Barra Natalina",
+            "dsc": "Deliciosa Barra de Chocolate presenteável, no sabores brigadeiro gourmet ou ninho com nutella.",
+            "price": 42
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+            "img": "./img/cardapio/sobremesas/pavedelimao.jpg",
+            "name": "Pavê de Limão",
+            "dsc": "Delicioso pavê com base de biscoito de maisena, creme aveludado de limão e cobertura de chantyninho.",
+            "price": 85
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+            "img": "./img/cardapio/sobremesas/pavedemaracuja.jpg",
+            "name": "Pavê de Maracujá",
+            "dsc": "Delicioso pavê com base de biscoito de maisena, creme aveludado de maracujá e cobertura de ganache de chocolate.",
+            "price": 85
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+            "img": "./img/cardapio/sobremesas/banoffe.jpg",
+            "name": "Banoffe",
+            "dsc": "Deliciosa banoffe, com massa de biscoito amanteigada, doce de leite, banana caramelada e chantilly.",
+            "price": 85
+        },/*
+        {
+            "id": "choose-your-own-ice-cream-donuts-6-pack",
+            "img": "./img/cardapio/sobremesas/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
+            "name": "Elegant Desserts",
+            "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
+            "price": 69
+        },
+        {
+            "id": "17481-jewish-dessert-3-pack",
+            "img": "./img/cardapio/sobremesas/jewish-classics-dessert-pack.7d4b76630f2fe75dcb7bbcf2501b7390.jpg",
+            "name": "Ess-a-Bagel",
+            "dsc": "Jewish Classics Dessert Pack",
+            "price": 89.95
+        },
+        {
+            "id": "dessert-bar-care-package",
+            "img": "./img/cardapio/sobremesas/dessert-bar-care-package.324aa28fe98c9dc67b75aac43376902e.jpg",
+            "name": "Bread and Roses Bakery",
+            "dsc": "Dessert Bar Care Package",
+            "price": 65
+        },
+        {
+            "id": "donut-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/donut-cookies-12-pack.46f42c61c4a79fd2051a99b2f23e890e.jpg",
+            "name": "Stan's Donuts",
+            "dsc": "Donut Cookies - 12 Pack",
+            "price": 49
+        },
+        {
+            "id": "gulab-jamun-ice-cream-cakes-2-pack",
+            "img": "./img/cardapio/sobremesas/gulab-jamun-ice-cream-cakes-2-pack.c45b4d0750ad22d741f84dc1f26d20e7.jpg",
+            "name": "Malai Ice Cream",
+            "dsc": "Gulab Jamun Ice Cream Cakes - 2 Pack",
+            "price": 79
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-12-pack.3b373bdd67cd25084182c21499f675d1.jpg",
+            "name": "Jacques Torres Chocolate",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 12 Pack",
+            "price": 69.95
+        }*/
+    ],
+    "churrasco": [
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio/churrasco/carolina-bbq-oink-sampler.1340b5a10cedc238cb2280306dd1d5a5.jpg",
+            "name": "Camarão - 1.200g",
+            "dsc": "Delicioso empadão, com massa que derrete na boca e recheio cremoso de camarão com catupiry - Apróx. 1.200g",
+            "price": 95
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img": "./img/cardapio/churrasco/bbq-meat-lovers-special-for-10.36ca670fda4bfa783c2ea9165e068d26.jpg",
+            "name": "Frango - 1.200g",
+            "dsc": "Empadão de Frango com massa que derrete na boca e recheio cremoso de frango com catupiry - Apróx. 1.200g ",
+            "price": 85
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+            "img": "./img/cardapio/churrasco/the-big-ugly-bbq-dinner-for-6.2dfae7818811adddce85cc1a910881a0.jpg",
+            "name": "Carne Seca - 1.200g",
+            "dsc": "Delicioso empadão, com massa que derrete na boca e recheio cremoso de carne seca com catupiry - Apróx. 1.200g",
+            "price": 100
+        }
+
+    ]
+    
+}
